92125b6e0437f7da29ef6d3c100b844bb10b3d2c
bogdan <belogur.bi@intechs.by>
2022-01-26 15:43:24 +0300

    add html test in func_test

--- TDD/func_test/func_test.py
+++ TDD/func_test/func_test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 
 class NewVisitorTest(unittest.TestCase):
     ''' test new visitor '''
@@ -20,6 +21,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000/')
         # проверка на наличие нужного заголовка
         self.assertIn('To-do', self.browser.title, 'Failure')
+        # find headertext h1
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-do', header_text)
+        # send word in input
+        input_box = self.browser.find_element_by_id('new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter to-do item')
+        input_box.send_keys('Купить носки')
+        time.sleep(2)
+        input_box.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('list_item')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == 'Купить носки' for row in rows))
+        self.fail('Close test')
+
+
+
+
 
 
 
